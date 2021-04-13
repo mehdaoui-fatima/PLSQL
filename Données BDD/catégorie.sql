be99d50a8b46e6a5422bac17ed48857f5920ba04
mehdaoui-fatima <mehdaoui.fa@gmail.com>
2021-04-13 12:29:59 +0000

    plsql project

---
+++ Données BDD/catégorie.sql
@@ -0,0 +1,8 @@
+Insert into CATÉGORIE (CODECATÉGORIE,NOMDECATÉGORIE) values (compteur.nextval,'Boissons');
+Insert into CATÉGORIE (CODECATÉGORIE,NOMDECATÉGORIE) values (compteur.nextval,'Condiments');
+Insert into CATÉGORIE (CODECATÉGORIE,NOMDECATÉGORIE) values (compteur.nextval,'Desserts');
+Insert into CATÉGORIE (CODECATÉGORIE,NOMDECATÉGORIE) values (compteur.nextval,'Produits laitiers');
+Insert into CATÉGORIE (CODECATÉGORIE,NOMDECATÉGORIE) values (compteur.nextval,'Pâtes et céréales');
+Insert into CATÉGORIE (CODECATÉGORIE,NOMDECATÉGORIE) values (compteur.nextval,'Viandes');
+Insert into CATÉGORIE (CODECATÉGORIE,NOMDECATÉGORIE) values (compteur.nextval,'Produits secs');
+Insert into CATÉGORIE (CODECATÉGORIE,NOMDECATÉGORIE) values (compteur.nextval,'Poissons et fruits de mer');
